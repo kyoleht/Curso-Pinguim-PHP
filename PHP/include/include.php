fa45ddae68db2f3f7091b33d7989a07a9383120e
kyoleht <rafaelferreiradasilva1812@gmail.com>
2024-05-11 19:36:08 -0300

    Criação do arquivo head.php

---
+++ PHP/include/include.php
@@ -0,0 +1,27 @@
+<?php 
+    declare(strict_types = 1);
+
+
+/**
+ * 
+ * include
+ * 
+ * lança um warning
+ * 
+ * é utilizado para incluir arquivos necessários no projeto
+ * 
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'head.php'; ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include</title>
+</head>
+<body>
+    <h1><?php 'Include' ?></h1>
+</body>
+</html>
