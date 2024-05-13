44ca074866dfc506e5f7b5562b2341fe2c63cd2d
kyoleht <rafaelferreiradasilva1812@gmail.com>
2024-05-12 21:23:36 -0300

    /

--- PHP/include/include.php
+++ PHP/include/include.php
@@ -18,9 +18,32 @@
 <head>
     <?php include 'include/head.php'; ?>
     <?php $config = include('inc/config.php'); ?>
+    <?php include_once ('inc/greetings.php'); ?>
     <title>Include</title>
 </head>
 <body>
     <h1><?php 'Include' ?></h1>
+
+    <div>
+    <table>
+        <thead>
+            <tr>
+                <th>Usuário</th>
+                <th>Senha</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td><?= $config['username']?></td>
+                <td><?= $config['password']?></td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+
+    <div style="margin-top: 50px">
+        <h2>Olá, <?= $nome ?></h2>
+    </div>
 </body>
 </html>
--- PHP/require/require.php
+++ PHP/require/require.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 
 /**
  * 
- * comentarios blablabla
+ * require e require_once
+ * 
+ * lança um erro se não encontrado
  * 
  */
 
@@ -14,12 +16,34 @@ declare(strict_types = 1);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require 'require/head.php'; ?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require 'req/head_.php'; ?>
+    <?php $config = require('inc/config.php'); ?>
+    <?php require_once('req/greetings.php'); ?>
     <title>Require</title>
 </head>
 <body>
     <h1><?php 'Require' ?></h1>
+
+    <div>
+    <table>
+        <thead>
+            <tr>
+                <th>Usuário</th>
+                <th>Senha</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td><?= $config['username']?></td>
+                <td><?= $config['password']?></td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+
+    <div style="margin-top: 50px">
+        <h2>Olá, <?= $nome ?></h2>
+    </div>
 </body>
 </html>
