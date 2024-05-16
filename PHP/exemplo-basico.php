d177f0cf1455171fb00ec7feca43cbffd5f8219d
kyoleht <rafaelferreiradasilva1812@gmail.com>
2024-05-15 22:25:25 -0300

    //

---
+++ PHP/exemplo-basico.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exemplo Básico</h1>
+
+
+<?php
+
+function soma ($a, $b) {
+    return $a + $b;
+}
+
+$resultado = soma($a = 1, $b = 2);
+
+echo $resultado;
+
+
+?>
+
+
+</body>
+</html>
---
+++ PHP/funcoes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções</title>
+</head>
+<body>
+    <h1>Funções</h1>
+
+    <ul>
+        <li>
+            <a href="./exemplo-basico.php">Exemplo Básico</a>
+        </li>
+</ul>
+
+</body>
+</html>
